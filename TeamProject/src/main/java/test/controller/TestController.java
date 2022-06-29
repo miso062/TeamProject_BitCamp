b373aed2e97ac5b991f816275c4182f8f57dcd73
your_name <miso97410@gmail.com>
2022-06-29 11:17:10 +0900

    setting test environment

---
+++ TeamProject/src/main/java/test/controller/TestController.java
@@ -0,0 +1,15 @@
+package test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/test")
+public class TestController {
+
+	@GetMapping(value="miso")
+	public String miso() {
+		return "/test/miso";
+	}
+}
---
+++ TeamProject/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<input type="button" value="김미소" onclick="location.href='/TeamProject/test/miso'">
+</body>
+</html>
