82b1d5d45c613dfba96f5ee30ef5af9cb772c763
sudong1004 <suujung1004@gmail.com>
2022-07-13 00:01:20 +0900

    일단 ..

--- TeamProject/src/main/java/com/spring/TeamProject/MainController.java
+++ TeamProject/src/main/java/com/spring/TeamProject/MainController.java
@@ -1,14 +1,23 @@
 package com.spring.TeamProject;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import main.service.MainService;
 
 @Controller
 public class MainController {
+
+	@Autowired
+	MainService mainService;
 	
 	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String index(Model model) {
@@ -20,5 +29,12 @@ public class MainController {
 		return "index";
 	}
 	
+	@PostMapping(value="getProductList")
+	@ResponseBody
+	public Map<String, Object> getnoticeList(@RequestParam(required = false, defaultValue = "1") String num){
+		System.out.println(num);
+		Map<String, Object> map = mainService.getProductList(num);
+		return map;
+	}
 	 
 }
