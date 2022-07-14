f70cdfb0ca81fd6a4787f1c5267f66b1148ae534
eunji <j-i@nate.com>
2022-07-14 16:54:11 +0900

    수정

--- TeamProject/src/main/java/user/controller/UserController.java
+++ TeamProject/src/main/java/user/controller/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import user.bean.UserDTO;
-import user.service.RandomPassword;
 import user.service.UserService;
 
 @Controller
