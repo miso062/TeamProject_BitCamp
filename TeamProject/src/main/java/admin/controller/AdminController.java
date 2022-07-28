82ff066ed87f148a6a5e74ce4785c4a97ce91545
sudong1004 <suujung1004@gmail.com>
2022-07-28 16:52:25 +0900

    admin 관리자만 로그인

--- TeamProject/src/main/java/admin/controller/AdminController.java
+++ TeamProject/src/main/java/admin/controller/AdminController.java
@@ -21,6 +21,7 @@ import admin.bean.AdminPaging;
 import admin.bean.AdminQnADTO;
 import admin.service.AdminService;
 import user.bean.UserDTO;
+import user.service.UserService;
 
 @Controller
 @RequestMapping(value="/admin")
@@ -28,6 +29,9 @@ public class AdminController {
 	
 	@Autowired
 	AdminService adminService;
+	
+	@Autowired
+	UserService userService;
 	
 	@GetMapping(value="notice")
 	public String notice(@RequestParam (required = false, defaultValue = "1") String pg, Model model) {
@@ -209,9 +213,15 @@ public class AdminController {
 	}
 	
 	@GetMapping(value="/")
-	public String admin(Model model) {
-		model.addAttribute("content", "/WEB-INF/admin/admin.jsp");
-		return "/admin/adminMain";
+	public String admin(Model model, HttpSession session) {
+		String user_id = (String) session.getAttribute("memId");
+		if(user_id == null || userService.getAuthor(user_id) != 10) {
+			model.addAttribute("content", "/WEB-INF/admin/adminLoginFail.jsp");
+			return "admin/adminLoginFail";
+		}else {
+			model.addAttribute("content", "/WEB-INF/admin/admin.jsp");
+			return "/admin/adminMain";				
+		}
 	}
 	
 	@GetMapping(value="/userManage")
